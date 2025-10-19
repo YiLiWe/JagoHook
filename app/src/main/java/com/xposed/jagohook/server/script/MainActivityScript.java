d04e7c2c8259233f09313aa6ffca9a3557408003
29584 <2334949001@qq.com>
2025-10-19 22:16:10 +0800

    输入密码

--- app/src/main/java/com/xposed/jagohook/server/script/MainActivityScript.java
+++ app/src/main/java/com/xposed/jagohook/server/script/MainActivityScript.java
@@ -11,15 +11,22 @@ public class MainActivityScript extends BaseScript {
     @Override
     public void onCreate(SuShellService suShellService, List<SuShellService.UiXmlParser.Node> nodes) {
         Map<String, SuShellService.UiXmlParser.Node> map = NodeScriptUtils.toContentDescMap(nodes);
-        String pass = "115599";
-        for (int i = 0; i < pass.length(); i++) {
-            String key = String.valueOf(pass.charAt(i));
-            if (map.containsKey(key)) {
-                Logs.d("找到按钮" + key);
-                SuShellService.UiXmlParser.Node node = map.get(key);
-                suShellService.click(node.getBounds());
+        inputPassword(suShellService, map);
+    }
+
+    //输入密码
+    private void inputPassword(SuShellService suShellService, Map<String, SuShellService.UiXmlParser.Node> map) {
+        if (map.containsKey("Masukkan PIN kamu")) {
+            String pass = "115599";
+            for (int i = 0; i < pass.length(); i++) {
+                String key = String.valueOf(pass.charAt(i));
+                if (map.containsKey(key)) {
+                    Logs.d("找到按钮" + key);
+                    SuShellService.UiXmlParser.Node node = map.get(key);
+                    suShellService.click(node.getBounds());
+                }
             }
+            Logs.d("执行完毕");
         }
-        Logs.d("执行完毕");
     }
 }
