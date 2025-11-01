57bdc241c9497c37f28ddce2d16dd0c39f92b2b7
29584 <2334949001@qq.com>
2025-11-01 13:40:44 +0800

    修改模块

--- app/src/main/java/com/xposed/jagohook/hook/MainHook.java
+++ app/src/main/java/com/xposed/jagohook/hook/MainHook.java
@@ -1,12 +1,18 @@
 package com.xposed.jagohook.hook;
 
+import android.app.Activity;
+import android.app.Application;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
 import android.util.Log;
-import android.view.View;
-import android.view.ViewGroup;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 
 import de.robv.android.xposed.IXposedHookLoadPackage;
 import de.robv.android.xposed.XC_MethodHook;
-import de.robv.android.xposed.XposedBridge;
 import de.robv.android.xposed.XposedHelpers;
 import de.robv.android.xposed.callbacks.XC_LoadPackage;
 
@@ -14,56 +20,49 @@ public class MainHook implements IXposedHookLoadPackage {
 
     @Override
     public void handleLoadPackage(XC_LoadPackage.LoadPackageParam lpparam) throws Throwable {
-        XposedBridge.hookAllMethods(lpparam.classLoader.loadClass("com.android.server.am.ActivityManagerService"), "updateActivityUsageStats", new XC_MethodHook() {
+        XposedHelpers.findAndHookMethod("android.app.Instrumentation", lpparam.classLoader, "callApplicationOnCreate", Application.class, new XC_MethodHook() {
             @Override
             protected void beforeHookedMethod(MethodHookParam param) throws Throwable {
-                super.beforeHookedMethod(param);
-                Log.e("代码", "切换界面");
+                if (param.args[0] instanceof Application application) {
+                    application.registerActivityLifecycleCallbacks(new Application.ActivityLifecycleCallbacks() {
+                        @Override
+                        public void onActivityCreated(@NonNull Activity activity, @Nullable Bundle bundle) {
+                            Log.d("代码", activity.getClass().getName());
+                        }
+
+                        @Override
+                        public void onActivityDestroyed(@NonNull Activity activity) {
+
+                        }
+
+                        @Override
+                        public void onActivityPaused(@NonNull Activity activity) {
+
+                        }
+
+                        @Override
+                        public void onActivityResumed(@NonNull Activity activity) {
+
+                        }
+
+                        @Override
+                        public void onActivitySaveInstanceState(@NonNull Activity activity, @NonNull Bundle bundle) {
+
+                        }
+
+                        @Override
+                        public void onActivityStarted(@NonNull Activity activity) {
+
+                        }
+
+                        @Override
+                        public void onActivityStopped(@NonNull Activity activity) {
+
+                        }
+                    });
+                }
             }
         });
-
-
-        XposedBridge.hookAllMethods(lpparam.classLoader.loadClass("com.android.server.accessibility.AccessibilityManagerService"), "sendAccessibilityEvent", new XC_MethodHook() {
-            @Override
-            protected void afterHookedMethod(MethodHookParam param) throws Throwable {
-                super.afterHookedMethod(param);
-                Log.e("代码", "执行");
-                // 打印调用类信息
-                Class<?> callerClass = param.method.getDeclaringClass();
-                Log.e("代码","调用类: " + callerClass.getName());
-
-                    // 打印方法参数
-                for (int i = 0; i < param.args.length; i++) {
-                    Object arg = param.args[i];
-                    Log.e("代码","参数 " + i + ": " +
-                            (arg != null ? arg.getClass().getName() : "null") +
-                            " = " + arg);
-                }
-
-
-                // 打印窗口属性（LayoutParams）
-                if (param.args.length > 1 && param.args[1] != null) {
-                    Object attrs = param.args[1];
-                    String windowTitle = (String) XposedHelpers.getObjectField(attrs, "mTitle");
-                    int width = XposedHelpers.getIntField(attrs, "width");
-                    int height = XposedHelpers.getIntField(attrs, "height");
-                    Log.e("代码","窗口标题: " + windowTitle + ", 尺寸: " + width + "x" + height);
-                }
-
-            }
-        });
-
-
     }
 
-    // 递归打印 View 层级
-    private void dumpViewHierarchy(View view) {
-        XposedBridge.log("View: " + view.getClass().getName());
-        if (view instanceof ViewGroup) {
-            ViewGroup group = (ViewGroup) view;
-            for (int i = 0; i < group.getChildCount(); i++) {
-                dumpViewHierarchy(group.getChildAt(i));
-            }
-        }
-    }
 }
