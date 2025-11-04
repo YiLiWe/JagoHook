efe25150277b51ba3411d926c6d81c03e776b3b8
29584 <2334949001@qq.com>
2025-11-04 10:53:11 +0800

    界面

--- app/src/main/java/com/xposed/jagohook/hook/MainHook.java
+++ app/src/main/java/com/xposed/jagohook/hook/MainHook.java
@@ -1,6 +1,12 @@
 package com.xposed.jagohook.hook;
 
+import android.app.Activity;
+import android.app.Application;
+import android.os.Bundle;
 import android.util.Log;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 
 import de.robv.android.xposed.IXposedHookLoadPackage;
 import de.robv.android.xposed.XC_MethodHook;
@@ -12,13 +18,47 @@ public class MainHook implements IXposedHookLoadPackage {
 
     @Override
     public void handleLoadPackage(XC_LoadPackage.LoadPackageParam lpparam) throws Throwable {
-        // Hook ClassLoader 的 loadClass 方法
-        XposedBridge.hookAllMethods(ClassLoader.class, "loadClass", new XC_MethodHook() {
+        XposedHelpers.findAndHookMethod("android.app.Instrumentation", lpparam.classLoader, "callApplicationOnCreate", Application.class, new XC_MethodHook() {
             @Override
             protected void beforeHookedMethod(MethodHookParam param) throws Throwable {
-                super.beforeHookedMethod(param);
-                String className = (String) param.args[0];
-                Log.d("代码", className);
+                if (param.args[0] instanceof Application application) {
+                    application.registerActivityLifecycleCallbacks(new Application.ActivityLifecycleCallbacks() {
+                        @Override
+                        public void onActivityCreated(@NonNull Activity activity, @Nullable Bundle bundle) {
+                            Log.d("代码", activity.getClass().getName());
+                        }
+
+                        @Override
+                        public void onActivityDestroyed(@NonNull Activity activity) {
+
+                        }
+
+                        @Override
+                        public void onActivityPaused(@NonNull Activity activity) {
+
+                        }
+
+                        @Override
+                        public void onActivityResumed(@NonNull Activity activity) {
+
+                        }
+
+                        @Override
+                        public void onActivitySaveInstanceState(@NonNull Activity activity, @NonNull Bundle bundle) {
+
+                        }
+
+                        @Override
+                        public void onActivityStarted(@NonNull Activity activity) {
+
+                        }
+
+                        @Override
+                        public void onActivityStopped(@NonNull Activity activity) {
+
+                        }
+                    });
+                }
             }
         });
     }
