4ebdd8db553a607028e123b47e7a154d13bcf51e
29584 <2334949001@qq.com>
2025-11-04 09:34:44 +0800

    获取加载的类

--- app/src/main/java/com/xposed/jagohook/hook/MainHook.java
+++ app/src/main/java/com/xposed/jagohook/hook/MainHook.java
@@ -4,6 +4,7 @@ import android.util.Log;
 
 import de.robv.android.xposed.IXposedHookLoadPackage;
 import de.robv.android.xposed.XC_MethodHook;
+import de.robv.android.xposed.XposedBridge;
 import de.robv.android.xposed.XposedHelpers;
 import de.robv.android.xposed.callbacks.XC_LoadPackage;
 
@@ -12,19 +13,14 @@ public class MainHook implements IXposedHookLoadPackage {
     @Override
     public void handleLoadPackage(XC_LoadPackage.LoadPackageParam lpparam) throws Throwable {
         // Hook ClassLoader 的 loadClass 方法
-        XposedHelpers.findAndHookMethod(
-                ClassLoader.class,
-                "loadClass",
-                String.class,
-                boolean.class,
-                new XC_MethodHook() {
-                    @Override
-                    protected void beforeHookedMethod(MethodHookParam param) throws Throwable {
-                        String className = (String) param.args[0];
-                        Log.d("代码", className);
-                    }
-                }
-        );
+        XposedBridge.hookAllMethods(ClassLoader.class, "loadClass", new XC_MethodHook() {
+            @Override
+            protected void beforeHookedMethod(MethodHookParam param) throws Throwable {
+                super.beforeHookedMethod(param);
+                String className = (String) param.args[0];
+                Log.d("代码", className);
+            }
+        });
     }
 
 }
