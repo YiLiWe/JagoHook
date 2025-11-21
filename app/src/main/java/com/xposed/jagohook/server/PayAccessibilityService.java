b8e048eb70266fbeaed1a67f3106e58a9fedb3fa
29584 <2334949001@qq.com>
2025-11-21 19:02:38 +0800

    等待支付完成

--- app/src/main/java/com/xposed/jagohook/server/PayAccessibilityService.java
+++ app/src/main/java/com/xposed/jagohook/server/PayAccessibilityService.java
@@ -341,17 +341,17 @@ public class PayAccessibilityService extends AccessibilityService {
 
 
         //等待转账状态
-/*        if (nodeInfoMap.containsKey("Uang Berhasil Dikirim!")) {
+        if (nodeInfoMap.containsKey("Uang Berhasil Dikirim!")) {
             success(nodeInfoMap, takeLatestOrderBean1);
         }
-*/
+
         //转账界面
-        if (nodeInfoMap.containsKey("Memilih \"Oke\" di perangkat tidak akan membatalkan transaksi kamu. Notifikasi akan kamu terima, setelah uang berhasil dikirim.")) {
+/*        if (nodeInfoMap.containsKey("Memilih \"Oke\" di perangkat tidak akan membatalkan transaksi kamu. Notifikasi akan kamu terima, setelah uang berhasil dikirim.")) {
             success(nodeInfoMap, takeLatestOrderBean1);
             if (nodeInfoMap.containsKey("Oke")) {
                 clickButton(nodeInfoMap.get("Oke"));
             }
-        }
+        }*/
 
 
         //选择银行
